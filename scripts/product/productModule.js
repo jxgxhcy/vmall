899cb6d196f75baa9da57f3731ae7650247abdaa
sjq1111 <sjq.on@foxmail.com>
2017-10-30 21:36:41 +0800

    Initial commit

---
+++ scripts/product/productModule.js
@@ -0,0 +1,26 @@
+'use strict';
+var productModule = angular.module('productModule',[]);
+productModule.config(function($stateProvider) {
+    $stateProvider.state('basket', {
+        url: "/basket",
+        templateUrl: "views/product/basket.html"
+    }).state('info', {
+        url: "/info",
+        templateUrl: "views/product/info.html"
+    }).state('list', {
+        url: "/list",
+        templateUrl: "views/product/productlist.html"
+    }).state('jd', {
+        url: "/jd",
+        template:'这是家电页面!'
+    }).state('phone', {
+        url: "/phone",
+        template:'这是手机页面!'
+    }).state('book', {
+        url: "/book",
+        template:'这是图书页面!'
+    }).state('dy', {
+        url: "/dy",
+        template:'这是日用页面!'
+    });
+});
